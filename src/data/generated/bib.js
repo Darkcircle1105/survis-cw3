93c5f909ab2588ac723becc918989c54e03e7ce4
Darkcircle1105 <liuyqdarkcircle@gmail.com>
2025-05-16 10:33:26 +0100

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,92 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+const generatedBibEntries = {
+    "adisak2017": {
+        "author": "Suwannahong, Adisak and Mekprasert, Tarinee",
+        "journal": "arXiv preprint arXiv:1704.04879",
+        "keywords": "genetic_algorithm, sport_scheduling",
+        "title": "A Sport Tournament Scheduling by Genetic Algorithm with Swapping Method",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "url": "https://arxiv.org/abs/1704.04879",
+        "year": "2017"
+    },
+    "andrew2003": {
+        "author": "Goldberg, Andrew B.",
+        "journal": "Master\u2019s thesis, University of Wisconsin-Madison",
+        "keywords": "genetic_algorithm, constraint_handling",
+        "title": "Highly Constrained Sports Scheduling With Genetic Algorithms",
+        "type": "article",
+        "url": "https://pages.cs.wisc.edu/~goldberg/publications/AndrewBGoldbergThesis.pdf",
+        "year": "2003"
+    },
+    "andrew2006": {
+        "author": "Lim, Andrew and Rodrigues, Bin and Zhang, Xiaodong",
+        "journal": "European Journal of Operational Research",
+        "keywords": "multi_venue, scheduling",
+        "title": "Scheduling Sports Competitions at Multiple Venues: Revisited",
+        "type": "article",
+        "url": "https://www.sciencedirect.com/science/article/abs/pii/S0377221705003711",
+        "year": "2006"
+    },
+    "jeanphilipe2001": {
+        "author": "Hamiez, Jean-Philippe and Hao, Jin-Kao",
+        "journal": "Lecture Notes in Computer Science",
+        "keywords": "tabu_search, league_optimization",
+        "title": "Solving the Sports League Scheduling Problem with Tabu Search",
+        "type": "article",
+        "url": "https://link.springer.com/chapter/10.1007/3-540-45612-0_2",
+        "year": "2001"
+    },
+    "kaveh2024": {
+        "author": "Sheibani, Kaveh and Alavi, Arash",
+        "journal": "Scientific Reports",
+        "keywords": "spectator_algorithm, innovation",
+        "title": "Application of a Novel Metaheuristic Algorithm Inspired by Stadium Spectators",
+        "type": "article",
+        "url": "https://www.nature.com/articles/s41598-024-53602-2",
+        "year": "2024"
+    },
+    "kelly2001": {
+        "author": "Easton, Kelly and Nemhauser, George and Trick, Michael",
+        "journal": "Principles and Practice of Constraint Programming",
+        "keywords": "constraint_programming, case_study",
+        "title": "Solving the Traveling Tournament Problem: A Case Study",
+        "type": "article",
+        "url": "https://citeseerx.ist.psu.edu/document?doi",
+        "year": "2001"
+    },
+    "luca2006": {
+        "author": "Di Gaspero, Luca and Schaerf, Andrea",
+        "journal": "Journal of Heuristics",
+        "keywords": "tabu_search, traveling_tournament",
+        "title": "A Composite-Neighborhood Tabu Search Approach to the Traveling Tournament Problem",
+        "type": "article",
+        "url": "https://link.springer.com/article/10.1007/s10732-006-9007-x",
+        "year": "2006"
+    },
+    "mka2022": {
+        "author": "Ariyaratne, M.K.A. and de Silva, R.M.",
+        "journal": "International Journal of Computer Science and Security",
+        "keywords": "metaheuristics, nature_inspired, review",
+        "title": "Meta-heuristics Meet Sports: A Systematic Review from the Viewpoint of Nature-Inspired Algorithms",
+        "type": "article",
+        "url": "https://sciendo.com/article/10.2478/ijcss-2022-0003",
+        "year": "2022"
+    },
+    "rasmussen2008": {
+        "author": "Rasmussen, Rasmus and Trick, Michael",
+        "journal": "European Journal of Operational Research",
+        "keywords": "round_robin, survey",
+        "title": "Round-Robin Tournament Scheduling: A Survey",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.ejor.2006.10.046",
+        "year": "2008"
+    },
+    "trick2003": {
+        "author": "Trick, Michael",
+        "journal": "Practice and Theory of Automated Timetabling",
+        "keywords": "sports_scheduling, tutorial",
+        "title": "Sports Scheduling: Problems and Applications",
+        "type": "article",
+        "url": "http://mat.gsia.cmu.edu/trick/talks/patat03.pdf",
+        "year": "2003"
     }
 };
